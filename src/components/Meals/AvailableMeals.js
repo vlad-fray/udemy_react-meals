a5066ba85758cc66035797274992588e2f815533
VFray <rekrut313@gmail.com>
2021-08-12 13:25:07 +0300

    fetch demo

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -49,3 +49,49 @@ const AvailableMeals = () => {
 };
 
 export default AvailableMeals;
+
+// async function fetchMealsHandler() {
+// 	let data = null;
+// 	try {
+// 		const res = await fetch(
+// 			'https://react-fetch-e6fb1-default-rtdb.europe-west1.firebasedatabase.app/meals.json'
+// 		);
+// 		console.log(res);
+// 		// if (!res.ok) throw new Error('Unvalid fetching data');
+// 		data = await res.json();
+// 		return data;
+// 	} catch (err) {
+// 		console.error(err);
+// 	}
+// }
+
+// async function addMealHandler(meal) {
+// 	try {
+// 		const check = await fetchMealsHandler();
+// 		console.log(check);
+// 		if (check) return;
+
+// 		const res = await fetch(
+// 			'https://react-fetch-e6fb1-default-rtdb.europe-west1.firebasedatabase.app/meals.json',
+// 			{
+// 				method: 'POST',
+// 				body: JSON.stringify(meal),
+// 				headers: {
+// 					'Content-Type': 'application/json',
+// 				},
+// 			}
+// 		);
+// 		if (!res.ok) throw new Error('wow');
+// 		const data = await res.json();
+// 		console.log(data);
+// 	} catch (err) {
+// 		console.error(err);
+// 	}
+// }
+
+// addMealHandler({
+// 	id: 'm1',
+// 	name: 'Sushi',
+// 	description: 'Finest fish and veggies',
+// 	price: 22.99,
+// });
